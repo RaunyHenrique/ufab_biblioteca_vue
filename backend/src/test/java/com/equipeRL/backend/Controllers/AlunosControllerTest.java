b2a2949ba73739454a7057b0bb75746f1b942cca
rauny henrique <raunyhenrique@iMac-de-rauny.local>
2018-06-15 14:43:31 -0300

    Fix para testes em controllers

--- backend/src/test/java/com/equipeRL/backend/Controllers/AlunosControllerTest.java
+++ backend/src/test/java/com/equipeRL/backend/Controllers/AlunosControllerTest.java
@@ -1,11 +1,21 @@
 package com.equipeRL.backend.Controllers;
 
 import com.equipeRL.backend.DefaultTest;
+import com.equipeRL.backend.Models.Permissao;
+import com.equipeRL.backend.Models.Usuario;
+import io.restassured.authentication.FormAuthConfig;
 import org.apache.http.HttpStatus;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
@@ -14,31 +24,28 @@ import static org.hamcrest.Matchers.is;
 
 public class AlunosControllerTest extends DefaultTest {
 
-//    @Test
-//    public void login() {
+    @Test
+    public void getAllAlunos() {
+
+        //fazer login
+//        Set<Permissao> permissoes = new HashSet<>();
+//        Permissao p = new Permissao();
+//        p.setId(1L);
+//        p.setNome("ROLE_ADMIN");
+//        permissoes.add(p);
 //
-//        //fazer login
-//        Map<String,String> user = new HashMap<>();
-//        user.put("username", "admin@email.com");
-//        user.put("password", "admin");
-//
+//        Usuario user = new Usuario("5465465", "dasdasdasd", "5454545", "dasdasdad", "dasdasdasd", "545646", "admin@email.com", "admin", permissoes);
+
 //        given()
 //                .contentType("application/json")
 //                .body(user)
-//                .when().post("api/login").then()
-//                .statusCode(HttpStatus.SC_OK)
-//                .body("empty", equalTo(false));
-//
-//    }
+//                .when().post("/usuarios").then()
+//                .statusCode(HttpStatus.SC_OK).log();
 
-//    @Test
-//    public void getAllAlunos() {
-//
-//        when()
-//                .get("/alunos")
-//                .then()
-//                .statusCode(HttpStatus.SC_OK);
-//
-//    }
+        given()
+                .when().get("http://localhost:8080/api/usuarios")
+                .then().statusCode(HttpStatus.SC_OK).log().all();
+
+    }
 
 }
--- backend/src/test/java/com/equipeRL/backend/DefaultTest.java
+++ backend/src/test/java/com/equipeRL/backend/DefaultTest.java
@@ -11,14 +11,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.HashMap;
 import java.util.Map;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.*;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest()
+@SpringBootTest(classes = BackendApplication.class)
 public class DefaultTest {
 
     private String basePath = "/api";
+
+    String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ7XCJlbWFpbFwiOlwiYWRtaW5AZW1haWwuY29tXCIsXCJhdXRoXCI6W3tcImF1dGhvcml0eVwiOlwiUk9MRV9BRE1JTlwifV19IiwiZXhwIjoxNTI5OTQ3ODE1fQ.eROz0mFaWvNo6PgCc6nfSxSl0W6teXNXMz3Ys-0RWS0sG4OBteZ66jqF_BU3RLLM-keHJlB0mNBMcG9N-nlctA";
 
     @Before
     public void setup() {
@@ -31,6 +32,9 @@ public class DefaultTest {
 
         //PORTA
         RestAssured.port = 8080;//8080?
+
+        //form("admin@email.com", "admin")
+        RestAssured.authentication = oauth2(token);
 
         System.out.println("CONFIGURAÇÃO BASE DOS TESTES: " + RestAssured.baseURI + " " + RestAssured.port + " " + RestAssured.basePath);
 
